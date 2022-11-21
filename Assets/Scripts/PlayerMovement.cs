d165c042b6f1e1c0d15f1c7861d8b0264d5642af
Borschik <grigriha@mail.ru>
2022-11-21 16:15:08 +0200

    Physic + gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Cooldowns))]
@@ -10,17 +11,27 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
 
     [SerializeField] private float rayDistance = 0.1f;
+    [SerializeField] private float gravityForce = 1;
+    [SerializeField] private float damping = 0.3f;
 
     [SerializeField] private float horizontalVelocity = 7;
+    [SerializeField] private float maximumHorizontalVelocity = 10;
     [SerializeField] private float horizontalVelocityOnAir = 2;
+    [SerializeField] private float normalizationVelocity = 0.4f;
+    [SerializeField] private float runToJumpImpulse = 0.5f;
+    [SerializeField] private float impulseAfterDash = 0.5f;
     [SerializeField] private float jumpVelocity = 8f;
     [SerializeField] private float dashVelocity = 17f;
-
     [SerializeField] private float dashDuration = 0.2f;
+    
+    [SerializeField] private Vector2 velocity;
+    
     [SerializeField] private bool jumping;
     [SerializeField] private bool onGround;
     public bool FaceLeft { get; private set; }
     private bool _dashUsed;
+    private bool _dashActive;
+    private bool _activeControl = true;
 
     [SerializeField] private float lastHorizontalMove;
 
@@ -36,43 +47,71 @@ public class PlayerMovement : MonoBehaviour
         _animator = GetComponent<Animator>();
         FaceLeft = false;
         _dashUsed = false;
+        _rigidbody.gravityScale = 0;
+
     }
 
     private void MoveHorizontally()
     {
         var dx = Input.GetAxisRaw("Horizontal");
         if (!onGround)
-        {
-            lastHorizontalMove += horizontalVelocityOnAir * dx * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                transform.position + Vector3.right * lastHorizontalMove * horizontalVelocity,
-                horizontalVelocity * Time.deltaTime);
-        }
-        
-        if(onGround)
-            lastHorizontalMove = dx;
+            MoveAndJump(dx);
+        else
+            MoveOnGround(dx);
+
+        if (dx == 0f) return;
+        FaceLeft = dx < 0f;
+    }
+
+    private void MoveAndJump(float dx)
+    {
+        lastHorizontalMove = Mathf.Clamp(lastHorizontalMove + horizontalVelocityOnAir * dx * Time.fixedDeltaTime, -1, 1);
+        Move(lastHorizontalMove * horizontalVelocity);
+
+        lastHorizontalMove *= 1 - normalizationVelocity * Time.fixedDeltaTime;
+
+        if (Mathf.Abs(lastHorizontalMove) >= 1e-6 && !_dashActive)
+            _spriteRenderer.flipX = lastHorizontalMove < 0f;
+    }
+
+    private void MoveOnGround(float dx)
+    {
+        lastHorizontalMove = dx * runToJumpImpulse;
+        _spriteRenderer.flipX = FaceLeft;
 
         if (Mathf.Abs(dx) < 1e-6)
         {
             _animator.SetBool(Running, false);
             return;
         }
-
         _animator.SetBool(Running, true);
-        var velocity = onGround ? horizontalVelocity : horizontalVelocityOnAir;
-        transform.position = Vector3.MoveTowards(
-            transform.position,
-            transform.position + Vector3.right * dx * velocity,
-            velocity * Time.deltaTime);
-        FaceLeft = dx < 0f;
         
-        _spriteRenderer.flipX = FaceLeft;
+        Move(dx * horizontalVelocity);
+        
+    }
+
+    private void Move(float deltaMove)
+    {
+        if (Mathf.Abs(deltaMove) >= 1e-6 || !_dashActive)
+            velocity.x = Mathf.Clamp(velocity.x + deltaMove, -maximumHorizontalVelocity, maximumHorizontalVelocity);
+    }
+
+    private void ApplyGravity()
+    {
+        if (_dashActive) 
+        {
+            velocity.y = 0f;
+            return;
+        }
+        if (!onGround)
+            velocity.y -= gravityForce * Time.fixedDeltaTime;
+        else
+            velocity.y = -1f;
     }
 
     private void Jump()
     {
-        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpVelocity);
+        velocity.y = jumpVelocity;
     }
 
     private void DashStart()
@@ -81,9 +120,13 @@ public class PlayerMovement : MonoBehaviour
             return;
 
         _dashUsed = true;
+        _dashActive = true;
+        _activeControl = false;
+        velocity = Vector2.zero;
         _animator.SetBool(Dashing, true);
-        _rigidbody.gravityScale = 0;
-        _rigidbody.velocity = (FaceLeft ? Vector2.left : Vector2.right) * dashVelocity;
+        velocity.x = (FaceLeft ? -1 : 1) * dashVelocity;
+        _spriteRenderer.flipX = FaceLeft;
+
 
         StartCoroutine(Extensions.Delay(dashDuration, DashEnd));
     }
@@ -92,7 +135,10 @@ public class PlayerMovement : MonoBehaviour
     {
         _rigidbody.velocity = Vector2.zero;
         _animator.SetBool(Dashing, false);
-        _rigidbody.gravityScale = 1;
+        _activeControl = true;
+        _dashActive = false;
+        lastHorizontalMove =
+            (FaceLeft ? -1 : 1) * Mathf.Min(Mathf.Abs(impulseAfterDash), Mathf.Abs(lastHorizontalMove));
         _cooldowns.DashCooldownStart();
     }
 
@@ -107,36 +153,28 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         CheckGround();
-    }
-
-    private void Update()
-    {
-        
-
-        if (onGround && Input.GetButton("Jump"))
+        ApplyGravity();
+        if (_activeControl)
         {
-            Jump();
-        }
-        Fall();
-        
-        MoveHorizontally();
-        PullBackHorizontally();
+            if (onGround && Input.GetButton("Jump"))
+            {
+                Jump();
+            }
+            Fall();
+            MoveHorizontally();
+            _animator.SetBool(Jumping, jumping);
 
-        
-        _animator.SetBool(Jumping, jumping);
-
-        if (_cooldowns.DashReady && Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            DashStart();
+            if (_cooldowns.DashReady && !_dashUsed && Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                DashStart();
+            }
         }
+        
+        _rigidbody.MovePosition(_rigidbody.position + velocity * Time.fixedDeltaTime);
+        if (!_dashActive)
+            velocity.x *= (1 - damping * Time.fixedDeltaTime);
     }
 
-    private void PullBackHorizontally()
-    {
-        var hit = Physics2D.Raycast(_rigidbody.position, Vector2.down, rayDistance, LayerMask.GetMask("Walls"));
-        if(hit.collider)
-            _rigidbody.MovePosition(_rigidbody.position + 3 * Vector2.right * (FaceLeft?1:-1));
-    }
 
     private void Fall()
     {
